1a3d1f07b0ff2f3dd577a1ebc3ab7d458f3aa13b
s-jcs <shosundberg@gmail.com>
2016-12-07 00:25:34 +0900

    cleaning handler

--- lib/ruboty/handlers/hash.rb
+++ lib/ruboty/handlers/hash.rb
@@ -1,68 +1,19 @@
 module Ruboty
   module Handlers
-    class Alias < Base
-      NAMESPACE = "alias"
+    class Hash < Base
+      NAMESPACE = "hash"
 
       on(
-        /alias (?<from>.+?) -> (?<to>.+)\z/m,
+        /hash (?<from>.+?) -> (?<to>.+)\z/m,
         description: "Create alias message",
         name: "create",
       )
 
-      on(
-        /list alias\z/,
-        description: "List alias",
-        name: "list",
-      )
-
-      on(
-        /delete alias (?<from>.+)\z/m,
-        description: "Delete alias",
-        name: "delete",
-      )
-
-      on(
-        //,
-        description: "Resolve alias if registered",
-        name: "resolve",
-        hidden: true,
-      )
-
       def create(message)
-        from = message[:from]
-        to = message[:to]
-        table[from] = to
-        message.reply("Registered alias: #{from} -> #{to}")
-      end
-
-      def delete(message)
-        if table.delete(message[:from])
-          message.reply("Deleted")
-        else
-          message.reply("Not found")
-        end
-      end
-
-      def list(message)
-        message.reply(aliases, code: true)
-      end
-
-      def resolve(message)
-        from = message.body.gsub(prefix, "")
-        if aliased = table[from]
-          robot.receive(
-            message.original.merge(
-              body: "#{message.body[prefix]}#{aliased}"
-            )
-          )
-          true
-        else
-          false
-        end
+        message.reply("Registered hash: #{from} -> #{to}")
       end
 
       private
-
 
       def prefix
         Ruboty::Action.prefix_pattern(robot.name)
